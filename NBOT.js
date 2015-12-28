bbd8b804b4d81c175e376cc43795936b123f1df2
nexdon <nikola.petrovicnidza.mol@gmail.com>
2015-12-28 05:25:22 +0100

    chafdsa

--- NBOT.js
+++ NBOT.js
@@ -1,3 +1,4 @@
+$.post('http://chat.stackoverflow.com/chats/' + $("input[name='room']").val() + '/messages/new', {text: '**[NBOT]** I started NBOT!', fkey: fkey().fkey});
 function say1() {
 	$.post('http://chat.stackoverflow.com/chats/' + $("input[name='room']").val() + '/messages/new', {
 			text: '**[NBOT]** Read rules at http://yourphotomake.info/rules.', 
@@ -16,7 +17,7 @@ function say() {
 
 var divy = document.createElement('div');
 divy.className = 'nbot';
-divy.innerHTML = '<style>.nbot {position: relative; width: 350px; height: auto; border: 3px solid #333; background-color: #333;} .ntitle {color: #fff; font-size: 20px; font-weight: bold; margin-bottom: 0px; border-bottom: 1px solid white;} .ntitle:hover {text-decoration: none;} .nitem {cursor: pointer; font-size: 15px; color: #fff; position: absolute; font-weight: normal; margin-left: 0px; margin-top: 0px; transition: 0.2s;} .nitem:hover {background-color: #fff; color: #333; text-decoration: none; font-weight: normal;} input {background: #333; color: #fff; border: 2px solid #fff;}</style><a class="ntitle">NBOT</a> <a href="http://yourphotomake.info/cms/admin735" target="_blank" class="nitem" style="position: absolute; top: 0px; right: 0px;">VISIT CMS</a><br><a class="nitem" onclick="say1();">Rules message</a><br><a class="nitem" onclick="say2()">Welcome message</a><br><input id="ntb" type="text" placeholder="Your message"><input type="button" onclick="say()" value="Send">';
+divy.innerHTML = '<style>.nbot {position: relative; width: 350px; height: auto; border: 3px solid #333; background-color: #333;} .ntitle {color: #fff; font-size: 20px; font-weight: bold; margin-bottom: 0px; border-bottom: 1px solid white;} .ntitle:hover {text-decoration: none;} .nitem {cursor: pointer; font-size: 15px; color: #fff; position: absolute; font-weight: normal; margin-left: 0px; margin-top: 0px; transition: 0.2s;} .nitem:hover {background-color: #fff; color: #333; text-decoration: none; font-weight: normal;} .nbot input {background: #333; color: #fff; border: 2px solid #fff;}</style><a class="ntitle">NBOT</a> <a href="http://yourphotomake.info/cms/admin735" target="_blank" class="nitem" style="position: absolute; top: 0px; right: 0px;">VISIT CMS</a><br><a class="nitem" onclick="say1();">Rules message</a><br><a class="nitem" onclick="say2()">Welcome message</a><br><input id="ntb" type="text" placeholder="Your message"><input type="button" onclick="say()" value="Send">';
 document.getElementById('roomtitle').appendChild(divy);
 
 console.log("NBOT started! type nbot() for help!");
