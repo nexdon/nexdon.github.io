551452b776bae14e2c20fd5b5344949c6417b441
nexdon <nikola.petrovicnidza.mol@gmail.com>
2015-12-28 16:34:26 +0100

    Fixed issues.

--- NBOT.js
+++ NBOT.js
@@ -41,12 +41,11 @@ function send(text, cb, errored, throttleTime) {
 }
 window.botCommand = {
     test: function (id) {
-        send(':' + id + ' This is test message!');
+        send(':' + id + ' This is a test message!');
     }, cat: function (id) {
         send(':' + id + ' http://i.imgur.com/BqceIC5.gif');
     }, gandalf: function (id) {
         send(':' + id + ' http://i.imgur.com/HxuZr0e.gif');
-        send(':' + id + ' test');
     }, listcommands: function (id) {
         var commands = [];
         for (var command in botCommand) {
