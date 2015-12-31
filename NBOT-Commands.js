ac6657c8a4f7a917718c33b4102dac0a2d7342b9
nexdon <nikola.petrovicnidza.mol@gmail.com>
2015-12-31 23:29:02 +0100

    Removed old vars for "time"

--- NBOT-Commands.js
+++ NBOT-Commands.js
@@ -663,10 +663,3 @@ window.botCommand = {
         }
     }
 };
-        var currentdate = new Date(); 
-    var datetime = "Now: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " - "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
