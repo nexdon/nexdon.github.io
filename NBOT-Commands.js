abea385fb4708f6338c029fefc212809ef18e426
nexdon <nikola.petrovicnidza.mol@gmail.com>
2015-12-31 23:25:04 +0100

    Fixed "time" command

--- NBOT-Commands.js
+++ NBOT-Commands.js
@@ -619,8 +619,7 @@ window.botCommand = {
     }, rules: function (id) {
         send(':' + id + ' Read rules at http://yourphotomake.info/rules.');
     }, time: function () {
-        var currentdate = new Date(); 
-        send("Time: " currentdate.getHours() + ":"  + currentdate.getMinutes() + ":" + currentdate.getSeconds());
+        send("Time: " new Date().getTime());
     }, sniper: function (id, args) {
         args = args || '';
         if (!args.trim()) {
