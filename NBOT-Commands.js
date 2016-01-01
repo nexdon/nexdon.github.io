5593c3a9ca1b00e0825c2dce4a6a4dc3e367a66a
nexdon <nikola.petrovicnidza.mol@gmail.com>
2016-01-01 02:12:26 +0100

    Update NBOT-Commands.js

--- NBOT-Commands.js
+++ NBOT-Commands.js
@@ -619,7 +619,7 @@ window.botCommand = {
     }, rules: function (id) {
         send(':' + id + ' Read rules at http://yourphotomake.info/rules.');
     }, time: function (id, d) {
-        send(':' + id + ' Time: ' + (Date.UTC().getTime());
+        send(':' + id + ' Time: ' + Date());
     }, sniper: function (id, args) {
         args = args || '';
         if (!args.trim()) {
