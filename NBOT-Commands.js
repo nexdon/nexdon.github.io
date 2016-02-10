0d3bea91f5534feeb174eba138e0a0ba48278186
nexdon <nikola.petrovicnidza.mol@gmail.com>
2016-02-10 20:03:10 +0100

    Update NBOT-Commands.js

--- NBOT-Commands.js
+++ NBOT-Commands.js
@@ -652,9 +652,9 @@ var memes = {
     list: 'trollface \n yes \n no \n okay \n me_gusta \n me_gusta_creepy \n me_gangsta \n me_gusta_creepy \n ich_mag \n me_gusta_facedance \n mother_of_gusta \n troll_gusta \n me_trollista \n indeed \n facepalm \n spiderman \n badass \n you_dont_say \n troll_dance \n so_hardcore \n genius \n not_bad \n ba_dum_no \n ba_dum_tsss \n crying \n derp \n kitten \n sir \n jesus \n jackie_chan \n challenge_accepted \n lol \n gtfo \n slap \n omega_troll \n gasp \n cat_gasp \n troll_rapper \n wut \n cereal_guy \n cereal_guy_spitting \n gusta_okay \n u_jelly \n u_mad \n kill \n bitch_please \n fap \n like_a_boss \n knife_self \n haters_gonna_hate \n fuck_yeah \n boring_cat \n forever_alone \n forever_alone_dance \n high \n nyan_cat \n pretty_badass \n maximum_trolling \n trolling \n desk_flip \n pokerface \n kappa'
 };
 var blame = {
-    brick: ' 1' + random,
-    kill: ' 2' + random,
-    dog: ' 3' + random
+    brick: ' for throwing brick at ' + random,
+    kill: ', he became a dog!',
+    dog: ' for being ' + random
 }
 
 function getBlock(text) {
@@ -719,8 +719,9 @@ window.botCommand = {
         var arr = new Array();
         $("#present-users").children('li.present-user').each(function(){ arr.push($(this).find('img')[0].title); });
         var random = arr[Math.floor( Math.random() * arr.length )];
+        var random2 = Math.floor(Math.random()*textArray.length);
         var keys = Object.keys(blame);
-        send(':' + id + ' blame ' + args + blame[keys[ keys.length * Math.random() << 0]]);
+        send(':' + id + ' blame ' + args + blame[random2]);
     }, imagetext: function (id, args) {
         args = args || '';
         if (!args.trim()) {
