d99d3b5569acc33bf7276cdb879c0b640bb523ae
nexdon <nikola.petrovicnidza.mol@gmail.com>
2016-02-10 17:46:25 +0100

    Update NBOT-Commands.js

--- NBOT-Commands.js
+++ NBOT-Commands.js
@@ -692,8 +692,7 @@ window.botCommand = {
         }
     }, online: function () {
         var arr = new Array(); 
-        var online = $("#present-users").children('li.present-user').each(function(){ console.log($(this).find('img')[0].title); });
-        send(arr);
+        var online = $("#present-users").children('li.present-user').each(function(){ send($(this).find('img')[0].title); });
     }, kill: function (id, args) {
         args = args || '';
         if (!args.trim()) {
