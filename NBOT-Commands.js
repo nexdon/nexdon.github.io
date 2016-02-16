f426b6f3dcebaf7f42f90f306e2e6feaf062b3da
nexdon <nikola.petrovicnidza.mol@gmail.com>
2016-02-16 16:26:23 +0100

    Update NBOT-Commands.js

--- NBOT-Commands.js
+++ NBOT-Commands.js
@@ -764,7 +764,7 @@ window.botCommand = {
             send('Hello ' + args + '!');
         }
     }, tag: function (id, args) {
-        send(':' + id + ' [tag:' + args + ']');
+        send(':' + id + ' [tag:' + args.replace(/ /g, '') + ']');
     }, description: function (id) {
         send(':' + id + ' ' + $('#roomdesc').text());
     }, echo: function (id, args) {
