9abf253013a99fb1884882f6ca7cb33651104d64
nexdon <nikola.petrovicnidza.mol@gmail.com>
2016-02-21 12:19:40 +0100

    Update NBOT-Commands.js

--- NBOT-Commands.js
+++ NBOT-Commands.js
@@ -835,7 +835,7 @@ window.botCommand = {
         if (!args.trim()) {
             send(':' + id + ' Please add text!');
         } else {
-            send(':' + id + ' [Search "' + args + '" on Google]' + '(https://www.google.com/search?q=' + encodeURIComponent(args).replace(/ /g, '+') + ')');
+            send(':' + id + '\n' + '["' + args + '" - Google search]' + '(https://www.google.com/search?q=' + encodeURIComponent(args).replace(/ /g, '+') + ')' + '\n' + ' ["' + args + '" - Google images]' + '(https://www.google.com/search?q=' + encodeURIComponent(args).replace(/ /g, '+') + '&source=lnms&tbm=isch' + ')');
         }
     }, slap: function (id, args) {
         args = args || '';
